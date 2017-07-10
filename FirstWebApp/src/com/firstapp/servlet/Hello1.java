4edd2e2b8755767c15c8f186ee6ba7523c992891
jagankumarr1991 <jagan.kumar.r@citi.com>
2017-07-10 18:49:50 +0530

    commit Hello1.java file

---
+++ FirstWebApp/src/com/firstapp/servlet/Hello1.java
@@ -0,0 +1,11 @@
+package com.firstapp.servlet;
+
+public class Hello1 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("created hello1 progarm to test psuh to upstream !!");
+
+	}
+
+}
