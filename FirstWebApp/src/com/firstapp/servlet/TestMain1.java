47a6bb0db32deadfb8e714c40f223c9e6f41f7f8
jagankumarr1991 <jagan.kumar.r@citi.com>
2017-07-10 18:43:59 +0530

    this is testmain1.java file commiting.

---
+++ FirstWebApp/src/com/firstapp/servlet/TestMain1.java
@@ -0,0 +1,14 @@
+package com.firstapp.servlet;
+
+public class TestMain1 {
+
+	public static void main(String[] args) {
+
+		
+		for (int i = 0; i < 10; i++) {
+			System.out.println("value is " + i);
+			
+		}
+	}
+
+}
