6e10587fbbdb3b8464dd93415f7b91b59f39b5ba
jagankumarr1991 <jagan.kumar.r@citi.com>
2017-07-11 18:00:14 +0530

    added cpmments.

--- FirstWebApp/src/com/firstapp/servlet/TestMain1.java
+++ FirstWebApp/src/com/firstapp/servlet/TestMain1.java
@@ -1,16 +1,18 @@
 package com.firstapp.servlet;
 
+/**
+ * added comments
+ * @author jr38331
+ *
+ */
 public class TestMain1 {
 
 	public static void main(String[] args) {
 
-		
 		for (int i = 0; i < 10; i++) {
 			System.out.println("value is " + i);
 			
 		}
-		
-		System.out.println("logic ends.");
 	}
 
 }
