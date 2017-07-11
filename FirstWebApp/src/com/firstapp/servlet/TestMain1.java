d454f127d5561948d9b1b109ae3749738cf404e3
jagankumarr1991 <jagan.kumar.r@citi.com>
2017-07-11 21:41:42 +0530

    2loops

---
+++ FirstWebApp/src/com/firstapp/servlet/TestMain1.java
@@ -0,0 +1,24 @@
+package com.firstapp.servlet;
+
+/**
+ * added comments
+ * @author jr38331
+ *
+ */
+public class TestMain1 {
+
+	public static void main(String[] args) {
+
+		for (int i = 0; i < 10; i++) {
+			System.out.println("values are " + i);
+			
+		}
+		
+		for (int i = 0; i < 10; i++) {
+			System.out.println("values are " + i);
+			
+		}
+		
+	}
+
+}
