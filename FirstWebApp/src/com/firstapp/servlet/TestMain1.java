c830b777f0123fd0f32b814d5f3725e50342b342
jagankumarr1991 <jagan.kumar.r@citi.com>
2017-07-11 21:53:24 +0530

    1loop - changed iteration

--- FirstWebApp/src/com/firstapp/servlet/TestMain1.java
+++ FirstWebApp/src/com/firstapp/servlet/TestMain1.java
@@ -9,17 +9,8 @@ public class TestMain1 {
 
 	public static void main(String[] args) {
 
-		for (int i = 0; i < 10; i++) {
-			System.out.println("values are " + i);
-			
-		}
-		
-		for (int i = 0; i < 10; i++) {
-			System.out.println("values are " + i);
-			
-		}
-		
-		for (int i = 0; i < 10; i++) {
+		// changed the iteration
+		for (int i = 10; i >= 1; i--) {
 			System.out.println("values are " + i);
 			
 		}
