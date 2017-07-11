134d7d00dd9011b2a47f5190d3b4cbdb146109e1
jagankumarr1991 <jagan.kumar.r@citi.com>
2017-07-11 21:14:50 +0530

    comments commit.

--- FirstWebApp/src/com/firstapp/servlet/TestMain1.java
+++ FirstWebApp/src/com/firstapp/servlet/TestMain1.java
@@ -9,7 +9,7 @@ public class TestMain1 {
 
 	public static void main(String[] args) {
 
-		for (int i = 0; i < 10; i++) {
+	/*	for (int i = 0; i < 10; i++) {
 			System.out.println("values are " + i);
 			
 		}
@@ -17,7 +17,7 @@ public class TestMain1 {
 		for (int i = 0; i < 10; i++) {
 			System.out.println("values = " + i);
 			
-		}
+		}*/
 	}
 
 }
