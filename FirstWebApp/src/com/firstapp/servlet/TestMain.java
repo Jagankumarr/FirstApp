6430c9ba6fc2075ec177ed4dd8b2748f64d4c337
jagankumarr1991 <jagan.kumar.r@citi.com>
2017-07-11 21:06:28 +0530

    added class level comments

--- FirstWebApp/src/com/firstapp/servlet/TestMain.java
+++ FirstWebApp/src/com/firstapp/servlet/TestMain.java
@@ -1,12 +1,16 @@
 package com.firstapp.servlet;
 
+/**
+ * 
+ * @author jr38331
+ *
+ */
 public class TestMain {
 
 	public static void main(String[] args) {
 
-		
 		System.out.println("welcome to git learning !!!");
-		
+
 	}
 
 }
