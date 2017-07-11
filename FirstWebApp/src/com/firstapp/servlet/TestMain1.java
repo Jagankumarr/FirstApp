24cc61767ac81312e1288571660441426239f506
jagankumarr1991 <jagan.kumar.r@citi.com>
2017-07-11 17:51:45 +0530

    added comments.

--- FirstWebApp/src/com/firstapp/servlet/TestMain1.java
+++ FirstWebApp/src/com/firstapp/servlet/TestMain1.java
@@ -1,10 +1,14 @@
 package com.firstapp.servlet;
 
+/**
+ * added comments
+ * @author jr38331
+ *
+ */
 public class TestMain1 {
 
 	public static void main(String[] args) {
 
-		
 		for (int i = 0; i < 10; i++) {
 			System.out.println("value is " + i);
 			
