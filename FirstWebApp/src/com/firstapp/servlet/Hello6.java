68f23c8762b4c18128c3ae0a89b1973feb5dbc3a
jagankumarr1991 <jagan.kumar.r@citi.com>
2017-07-12 14:30:20 +0530

    merged file.

---
+++ FirstWebApp/src/com/firstapp/servlet/Hello6.java
@@ -0,0 +1,13 @@
+package com.firstapp.servlet;
+
+public class Hello6 {
+
+	public static void main(String[] args) {
+		display();
+	}
+	
+	static void display(){
+		System.out.println("display method.");
+	}
+
+}
