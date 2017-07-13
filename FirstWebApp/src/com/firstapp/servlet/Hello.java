be97252b6dd522fbeffcac61fda98091c4e33f8c
jagankumarr1991 <jagan.kumar.r@citi.com>
2017-07-13 16:21:28 +0530

    added m1 method.

--- FirstWebApp/src/com/firstapp/servlet/Hello.java
+++ FirstWebApp/src/com/firstapp/servlet/Hello.java
@@ -3,9 +3,11 @@ package com.firstapp.servlet;
 public class Hello {
 
 	public static void main(String[] args) {
-		
-		
 		System.out.println("Hello !!!");
+	}
+	
+	void m1(){
+		System.out.println("this is m1 method");
 	}
 
 }
